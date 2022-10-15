a768039727155f01745ca381a84d3f53880d4432
Atharva <atharvajoshi202@gmail.com>
2022-10-15 22:58:24 +0530

    Initial commit

---
+++ Components/Results.js
@@ -0,0 +1,164 @@
+function Results() {
+  return (
+    <div>
+      <section className="text-white-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer  transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/a29af9d3159d96bb259aba7403bd23d0/en/16x9/320x180/9e84b3843a8d2afe00aff38531c00f81_1920x1080.webp"
+                />
+              </a>
+
+              <div className="mt-4  ">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$16.00</p>
+              </div>
+            </div>
+
+            {/* -------------------------------- */}
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/f68cd89877494f8f30bebb9370fe80a1/en/16x9/320x180/e494c65cd88640e328e1904ec0cd8c24_1920x1080.webp"
+                />
+              </a>
+
+              <div className="mt-4  ">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1 ">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  Shooting Stars
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$21.15</p>
+              </div>
+            </div>
+            {/* ------------------------------------- */}
+
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/97b2a003d9bda14db85fdfa5042e9be8/en/16x9/320x180/b4ff2776bf2f58057d626f60492ea883_1920x1080.webp"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  Neptune
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$12.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/f12882877cac27687efb1e1cd0e87a92/en/16x9/320x180/test_pic1665548246409_badged_1665548249127.webp"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$18.40</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/07fe6b61a6c3dea88f619e8e5f032b74/en/16x9/320x180/test_pic1664772684317.webp"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$16.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/ca7a124443044bf3c1f8ea52c8b7f9fc/en/2x3/320x480/test_pic1665138968193.webp"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  Shooting Stars
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$21.15</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/fa83fdbb12a19e9b877c065c3d76d770/en/16x9/320x180/test_pic1657714648581_badged_1659566859105.webp"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  Neptune
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$12.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="https://qqcdnpictest.mxplay.com/pic/55edb7dbec6d59f19787047158f9fe3a/en/2x3/320x480/test_pic1665385215406.webp"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-white-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-white-900 title-font text-lg font-medium transition-all duration-100 ease-in-out group-hover:font-bold">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1 opacity-0 group-hover:opacity-100">$18.40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Results;
